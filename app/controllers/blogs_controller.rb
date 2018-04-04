336dfa7a66554a287dff01df22df1913086f3fcb
junwen yang <wsklxsx@126.com>
2018-04-04 02:49:33 +0000

    change users to not includes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@ class BlogsController < ApplicationController
    include BlogsHelper
    def show
       for i in 1..5
-         @users = User.includes(:blogs).limit(5)
+         @users = User.limit(5)
       end
       @blogs = Blog.blogByCategory('admin')
       render 'show'
