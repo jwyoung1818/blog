b6d519ec3aad8035c9f7ca74aba9afb4d3d21b81
junwen yang <wsklxsx@126.com>
2018-04-23 22:40:47 +0000

    change blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,11 @@
 
 class BlogsController < ApplicationController
    include BlogsHelper
+   respond_to :js, :json, :html
+   def index
+      Rails.logger.debug "----Out #{params}"
+      render :nothing => true
+   end
    def show
       for i in 1..5
          @users = User.limit(5)
